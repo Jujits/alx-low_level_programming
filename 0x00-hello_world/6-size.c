ec259658804de621c76e66aeeb1d22aaacfd598a
Jujits <mwangijosephmburu12@gmail.com>
2023-05-28 06:53:58 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,21 @@
+/*
+ *A statement to use size for the first time
+ */
+
 #include <stdio.h>
+/**
+ *Size is used to measure the size of the descriptor
+ *Return always 0
+ */
 
 int main(void)
 {
-	printf("Size of a char; %\n", sizeof((char));
+	printf("Size of a char; %zu bytes\n", sizeof(char));
+	printf("Size of int;%zu bytes\n", sizeof(int));
+	printf("Size of long int;%zu bytes\n", sizeof(long int));
+	printf("Size of long long int;%zu bytes\n", sizeof(long long int));
+	printf("Size of float;%zu bytes\n", sizeof(float));
+
 	return (0);
 	}
 
